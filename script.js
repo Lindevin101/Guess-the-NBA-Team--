113aa04f862207ed7f382931399ac4b5449c5269
dlin <lindevin101@gmail.com>
2023-10-13 13:50:47 -0400

    add some new things

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 let letters = document.querySelectorAll (`.letter`);
 let input = document.querySelector("#input");
+/*keyboard above*/
+
+let words = [
+    "HAWKS", "CELTICS", "NETS", "HORNETS", "BULLS", "CAVALIERS",
+    "MAVERICKS", "NUGGETS", "PISTONS", "WARRIORS", "ROCKETS",
+    "PACERS", "CLIPPERS", "LAKERS", "GRIZZLIES", "HEAT", "BUCKS",
+    "TIMBERWOLVES", "KNICKS", "THUNDER", "MAGIC", "SIXERS", "SUNS",
+    "TRAILBLAZERS", "KINGS", "SPURS", "RAPTORS", "JAZZ", "WIZARDS"
+];
+
+let answer = "";
+let guessedLetters = [];
+let usedWords = []; 
+
+let tries = document.querySelector(".remaining");
+let wins = document.querySelector(".dubs");
+let losses = document.querySelector(".wrong")
