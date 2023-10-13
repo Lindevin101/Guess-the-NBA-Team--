a69f6bf2527afad306e2fb51a6fc86dbb59a7d65
dlin <lindevin101@gmail.com>
2023-10-13 16:18:23 -0400

    did some JS

--- script.js
+++ script.js
@@ -16,4 +16,61 @@ let usedWords = [];
 
 let tries = document.querySelector(".remaining");
 let wins = document.querySelector(".dubs");
-let losses = document.querySelector(".wrong")
+let losses = document.querySelector(".wrong");
+
+let winScore = 0;
+let lossesScore = 0;
+
+function highlight (e) {
+    for (i = 0; i < letters.length; i++) {
+        if (letters[i].dataset.letter == e.key){
+            letters[i].classList.add("active");
+            answer = answer.toLowerCase();
+            let curLet = letters[i];
+            if(answer.includes(e.key)) {
+                let results = [];
+                letters[i].computedStyleMap.background = "green"
+                function finder() {
+                    let index = asnwer.indexOf(curLet.interHTML)
+                    while (index != -1){
+                        results.push(index);
+                        index = asnwer.indexOf(curLet.innerHTML, idex +1);
+                    }
+                    return results;
+                }
+                finder();
+                results.forEach((result) =>
+                newWord.splice(result, 1, curLet,innerHTML)
+                );
+                unknown.innerHTML = newWord.join("");
+                if (!unknown.innerHTML.includes("-") && tries.innerText > 0) {
+                    winScore += 1;
+                    wins.innerText = winScore;
+                    input.classList.add("hidden");
+                    unClick(e);
+                    alert(`YouWin! The word was ${answer}.`);
+                }
+            }else if (!answer.includes(e.key)) {
+                if(letters[i].computedStyleMap.background !== "red"){
+                    lerrers[i].style.background = "red";
+                    tries.innerText -= 1;
+                    if (tries.innerText < 1) {
+                        lossScore += 1;
+                        losses.innerText = lossScore;
+                        input.classList.add("hidden");
+                        unClick(e);
+                        alert (`You Lose! The Word was ${answer}.`);
+                    }
+                }
+            }
+        }
+    }
+}
+function unClick(e) {
+    for (i = 0; i< letter.length; i++) {
+        if(letters[i].dataset.letter == e.key){
+            letters[i].classLet.remove("active");
+        }
+        input.value = "";
+    }
+}
