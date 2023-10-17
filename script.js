806fb7c51c4dd64df7d01410d436f45506a46d2d
dlin <lindevin101@gmail.com>
2023-10-17 09:23:56 -0400

    fixed background, game is fully functional.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let letters = document.querySelectorAll (`.letter`);
-let input = document.querySelector("#input");
-/*keyboard above*/
+let letters = document.querySelectorAll('.letter');
+let input = document.querySelector('#input');
+/* Keyboard above */
 
 let words = [
     "HAWKS", "CELTICS", "NETS", "HORNETS", "BULLS", "CAVALIERS",
@@ -12,65 +12,124 @@ let words = [
 
 let answer = "";
 let guessedLetters = [];
-let usedWords = []; 
+let usedWords = [];
 
 let tries = document.querySelector(".remaining");
 let wins = document.querySelector(".dubs");
 let losses = document.querySelector(".wrong");
 
 let winScore = 0;
-let lossesScore = 0;
+let lossScore = 0;
 
-function highlight (e) {
-    for (i = 0; i < letters.length; i++) {
-        if (letters[i].dataset.letter == e.key){
+function highlight(e) {
+    for (let i = 0; i < letters.length; i++) {
+        if (letters[i].dataset.letter === e.key) {
             letters[i].classList.add("active");
+
+            // Store the correct key in the answer
             answer = answer.toLowerCase();
             let curLet = letters[i];
-            if(answer.includes(e.key)) {
+            if (answer.includes(e.key)) {
                 let results = [];
-                letters[i].computedStyleMap.background = "green"
+                letters[i].style.background = "green";
+
                 function finder() {
-                    let index = asnwer.indexOf(curLet.interHTML)
-                    while (index != -1){
+                    let index = answer.indexOf(curLet.innerHTML);
+                    while (index !== -1) {
                         results.push(index);
-                        index = asnwer.indexOf(curLet.innerHTML, idex +1);
+                        index = answer.indexOf(curLet.innerHTML, index + 1);
                     }
                     return results;
                 }
+
                 finder();
                 results.forEach((result) =>
-                newWord.splice(result, 1, curLet,innerHTML)
+                    newWord.splice(result, 1, curLet.innerHTML)
                 );
                 unknown.innerHTML = newWord.join("");
+
                 if (!unknown.innerHTML.includes("-") && tries.innerText > 0) {
                     winScore += 1;
                     wins.innerText = winScore;
                     input.classList.add("hidden");
                     unClick(e);
-                    alert(`YouWin! The word was ${answer}.`);
+                    alert(`You Win! The word was ${answer}.`);
                 }
-            }else if (!answer.includes(e.key)) {
-                if(letters[i].computedStyleMap.background !== "red"){
-                    lerrers[i].style.background = "red";
+            } else if (!answer.includes(e.key)) {
+                if (letters[i].style.background !== "red") {
+                    letters[i].style.background = "red";
                     tries.innerText -= 1;
                     if (tries.innerText < 1) {
                         lossScore += 1;
                         losses.innerText = lossScore;
                         input.classList.add("hidden");
                         unClick(e);
-                        alert (`You Lose! The Word was ${answer}.`);
+                        alert(`You Lose! The word was ${answer}.`);
                     }
                 }
             }
         }
     }
 }
+
 function unClick(e) {
-    for (i = 0; i< letter.length; i++) {
-        if(letters[i].dataset.letter == e.key){
-            letters[i].classLet.remove("active");
+    for (let i = 0; i < letters.length; i++) {
+        if (letters[i].dataset.letter === e.key) {
+            letters[i].classList.remove("active");
         }
         input.value = "";
     }
+}
+
+input.addEventListener("keydown", highlight);
+input.addEventListener("keyup", unClick);
+
+function randomizeWord() {
+    if (words.length === 0) {
+        console.log(usedWords);
+        words = usedWords;
+        console.log(words);
+        usedWords = [];
+    }
+    answer = words[Math.floor(Math.random() * words.length)];
+    return answer;
+}
+
+let unknown = document.querySelector(".unknown");
+let startBtn = document.querySelector(".start");
+let blankWord = "";
+let newWord = [];
+let count = document.querySelector(".count");
+
+function createBlank() {
+    clearBoardStyles();
+    tries.innerText = 6;
+    guessedLetters = [];
+    randomizeWord();
+    usedWords.push(answer);
+    for (let i = 0; i < words.length; i++) {
+        if (words[i] === answer) {
+            words.splice(i, 1);
+        }
+    }
+    blankWord = "";
+    for (let i = 0; i < answer.length; i++) {
+        blankWord += "-";
+    }
+    unknown.innerHTML = blankWord;
+
+    newWord = blankWord.split("");
+    count.innerText = answer.length;
+}
+
+createBlank();
+startBtn.addEventListener("click", createBlank);
+
+function clearBoardStyles() {
+    letters.forEach((letter) => {
+        letter.style.background = "";
+        letter.classList.remove("active");
+        input.value = "";
+        input.classList.remove("hidden");
+    });
 }
